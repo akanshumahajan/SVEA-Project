afba0714974560d64822e03fedca090247579268
Hackerman342 <53625197+Hackerman342@users.noreply.github.com>
2019-12-13 15:40:11 +0100

    Uploading files to run remote camera on SVEA0

--- aruco_detection/aruco_tf.py
+++ aruco_detection/aruco_tf.py
@@ -30,7 +30,20 @@ class ArucoTF():
         # Read fiducial id value
         param3 = rospy.search_param("fiducial_id")
         self.fid_id = rospy.get_param(param3)
-     
+        # Read estimated vehicle pose frame_id and topic
+        param4 = rospy.search_param("est_vehicle_frame")
+        self.est_veh_pose_frame = rospy.get_param(param4)
+        param5 = rospy.search_param("map_pose_estimate_topic")
+        self.est_veh_pose_top = rospy.get_param(param5)
+        # Read map frame   
+        param6 = rospy.search_param("map_frame")
+        self.map_frame = rospy.get_param(param6)
+        # Read covariance values
+        param7 = rospy.search_param("lin_cov_aruco")
+        self.lin_cov = rospy.get_param(param7)
+        param8 = rospy.search_param("ang_cov_aruco")
+        self.ang_cov = rospy.get_param(param8)
+
         # Initialize callback variables
         self.fid_pose = None
 
@@ -40,17 +53,28 @@ class ArucoTF():
         # Establish subscription to Fiducial Pose
         rospy.Subscriber(self.fid_pose_topic, FiducialTransformArray, self.fid_pose_callback)
         # Delay briefly for subscriber to find message
-        rospy.sleep(2)
+        rospy.sleep(0.5)
 
-    def tf_calc_and_pub(self):
+        # Initilize tf2 broadcaster and transform message
+        self.br = tf2_ros.TransformBroadcaster()
+        self.t = TransformStamped()
+        self.t.header.frame_id = self.cam_frame
+        self.t.child_frame_id = "fid"+str(self.fid_id)
+
+        # Initialize listener for estimated pose of vehicle in map frame
+        self.tfBuffer = tf2_ros.Buffer()
+        self.listener = tf2_ros.TransformListener(self.tfBuffer)
+
+        # Initialize publisher for estimated pose of vehicle in map frame
+        self.posepub = rospy.Publisher(self.est_veh_pose_top, PoseWithCovarianceStamped, queue_size=10)
+        self.pub = PoseWithCovarianceStamped()
+        self.pub.header.frame_id = self.map_frame
+        self.pub.pose.covariance = self.cov_matrix_build()
+
+
+    def tf_calc(self):
         # initialize old_time for comparison
         old_time = 0
-        # initilize broadcaster and message before loop
-        br = tf2_ros.TransformBroadcaster()
-        t = TransformStamped()
-        t.header.frame_id = self.cam_frame
-        t.child_frame_id = "fid"+str(self.fid_id)
-
 
         while not rospy.is_shutdown():
             # Use a temp value so self.fid_pose.transforms 
@@ -61,17 +85,31 @@ class ArucoTF():
             if time > old_time + .033 : # .033 sec = 30 hz
                 for i in range(len(temp)): # number of detected markers
                     if temp[i].fiducial_id == self.fid_id: # if we see desired marker
-                        t.header.stamp = rospy.Time.now()
-                        t.transform = temp[i].transform
-                        br.sendTransform(t)
+                        self.t.header.stamp = rospy.Time.now()
+                        self.t.transform = temp[i].transform
+                        self.br.sendTransform(self.t)
+                        # Call publish function so estimated base_link can be read in map frame
+                        self.map_pose_pub()
 
-                        #br.sendTransform((trans.x, trans.y, trans.z),(rot.x, rot.y, rot.z, rot.w),
-                                        #rospy.Time.now(), "fid"+str(self.fid_id), self.cam_frame)
+
+                ### Should old_time only be in the if statement?
+                # Seemed to work before when it was outside, but logically I think should be in...
+                old_time = time # update time for comparison on next iter
             
-            old_time = time # update time
+            #old_time = time # update time for comparison on next iter
 
-
-        
+    def map_pose_pub(self):
+        trans = None
+        try:
+            trans = self.tfBuffer.lookup_transform(self.map_frame, self.est_veh_pose_frame, rospy.Time(0), rospy.Duration(1.0))
+        except:
+            #trans = self.tfBuffer.lookup_transform(self.map_frame, self.est_veh_pose_frame, rospy.Time(0), rospy.Duration(1.0))
+            rospy.loginfo('Failure of lookup transfrom from estimated vehicle pose to map')
+        if trans:
+            self.pub.header.stamp = rospy.Time.now()
+            self.pub.pose.pose.position = trans.transform.translation
+            self.pub.pose.pose.orientation = trans.transform.rotation
+            self.posepub.publish(self.pub)            
 
     # Callback function for fiducial pose subscription (from aruco_detect)
     def fid_pose_callback(self, fid_pose_msg):
@@ -79,10 +117,14 @@ class ArucoTF():
         if self.fid_pose.transforms != []:
             self.fid_pose.header.frame_id = self.cam_frame
 
-
-
-
-
+    def cov_matrix_build(self):
+        self.cov_matrix = [self.lin_cov, 0.0, 0.0, 0.0, 0.0, 0.0,
+                            0.0, self.lin_cov, 0.0, 0.0, 0.0, 0.0,
+                            0.0, 0.0, self.lin_cov, 0.0, 0.0, 0.0,
+                            0.0, 0.0, 0.0, self.ang_cov, 0.0, 0.0,
+                            0.0, 0.0, 0.0, 0.0, self.ang_cov, 0.0,
+                            0.0, 0.0, 0.0, 0.0, 0.0, self.ang_cov]
+        return self.cov_matrix
 
 
 if __name__ == '__main__':
@@ -91,4 +133,5 @@ if __name__ == '__main__':
     rospy.loginfo("Successful initilization of node")
 
     tf_ar = ArucoTF()
-    tf_ar.tf_calc_and_pub()
+    tf_ar.tf_calc()
+
